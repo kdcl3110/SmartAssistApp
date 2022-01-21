de7a547f989d4e158798689c03f1fcb381eaecc9
kdcl3110 <diepeloic31@gmail.com>
2022-01-21 12:31:23 +0100

    initialisation du projet

---
+++ env.js
@@ -0,0 +1,14 @@
+export const dialogflowConfig = {
+  type: 'service_account',
+  project_id: 'virtualassist-vrkf',
+  private_key_id: '383235c28941ae4df18d8de6e0bf5f35def1bdc5',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC/KvrhQ2Rc++xY\nlc/A5zVrOzygej8hGru0fcKQJxP3VO8fNoGCErAD6Y6eWE1t5vC5T2VTclo4YCFm\npRXoywvJtqRfZr/ipgb1ecj/DiN879jI62OJrCPdRLd/gigDhsL0tN1njdXUr/vn\n7fUcfKgFWQluW6JfB+cAcEgfw0izzdergGd69ghMYcPh9JdIIiYixEWO5niANsw6\nz3cZD0tfBBHz4m9Z4RjWGiK5PdiaVbD/d1Xa2q6Acs/sACTlmET0Ng2jiKwo/S8g\nNES8zbtoaM8W5EfbjU/mNkcV4kHEsvkg74KffHQXiJaxVU21f6/EcDprlbSkwFjX\n0mnGlYutAgMBAAECggEAOU+J0gGxs3ylIPOcypCmP6UNKnErvuF0V4qCS1Qj1Ljm\nTT26esPed/XLl2YSpHmhPnTyhB9as98B9HNWi6lfudPjGGTF7w1mNa6SlwBmnDyN\nicKj3zC3ucyxlGzxUQlUJieY3sQzUGWICEKbArTObHgCb07ScVn9o3CF9tvFTn3J\nD1yIvB+1wEVhWEBRMJPtXMd5CnsM7ljPs+zdDZmRE2y5NKHbtbgo6GzcXzZ+drsR\neDym9xWq036VTi5Cmh4o88/EMhebCDzFzl1L6Nd+XEwXDtDU1Tt3m1MZX8cB1Iu7\nIoofX9Ln8IHPGYUEDV4NKR6GmEwsOBMnz09tlzNJcwKBgQDs7qol0MxYRfbLiZbO\nntv+eC0oQn39DcSjgHnjFuucyRuLV74aDa1ex7b1cfbzY7aLcZ7OuSl/47DwwD8B\nZ1E8dvhFpDtfBoxNcn76UTG1GXyCrppGsHdwHcRipjHJJQgRMcet33hZFMj+vEIm\nDJg/xZTXTLQMaEsuHsqyTvb+DwKBgQDOjXdn6u1rq/o0GPFCuxjaBR9XfU6lRF0J\nkiZl6PHrYcyA/VDy4jvPXXX5ixJqiUtbYCY1PmNgb4NWrOdJKDTryQgtW1ORI2hd\nubuyndwMuLsetQWpgQZsT3mXG2BECrfoP/mitDHKYL21m8f5ETl9x1d8Oyb+hZM/\nUQs/vh/WgwKBgBSvuddXZaAHa8bVmn8ry5VqNLchJxLjEtobq4iyRZwIywjdflGU\nBCEUWtsPOjqB6DxAjVcLP55U6Ov8PQQIW0VmNcMO1cg0MCfK6V89m5NuSDq4t+uY\nGLWHQ9BBJnOjchLeP8rkigNtFyd2lGfyDZDz1Uz++3Fh4b6DHbt3vYTtAoGBAIYu\n8uZIgSqUk7tqhLk0MKXZ9A1zMzc8HTDNiDJmTVr7ss4NQpw3CmHhNnYBudoKDTF0\nWf325LpOV0Vkl/A3tYGDnNNSZAyCsxdIRL55vO1qyOze7qm6T0DntHbM6xHo4KZ5\ngSQI6duuUy/63sNYREAI0zW6e8+XWRwl+yfSzH4fAoGAY/zjBaxdWCSf5/myNRgR\nloJZwUADLndF/p2DpyqWCwJpbJMAlN7SN8/aWQ1vZc22JJvtbaQmpmoyv9S3rT9l\nskBYHnEnAkBAeDK09/Zr6CbjeZeGTZ8nNcLFn+Byp7U8Py3vwD7MdmXyDm7qFeyW\ny0xzrwF2JsD0sizjaMifqAY=\n-----END PRIVATE KEY-----\n',
+  client_email: 'virtualassis@virtualassist-vrkf.iam.gserviceaccount.com',
+  client_id: '116864315101622831830',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/virtualassis%40virtualassist-vrkf.iam.gserviceaccount.com',
+};
